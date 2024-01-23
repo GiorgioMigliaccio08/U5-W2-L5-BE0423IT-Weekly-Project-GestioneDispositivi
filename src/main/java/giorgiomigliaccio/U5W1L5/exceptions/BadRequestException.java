152f439743238c41b22862fc2e86ecb88e6c34f2
Giorgio Migliaccio <giorgiomigliaccio08@gmail.com>
2024-01-23 16:24:19 +0100

    Commit 1

--- src/main/java/giorgiomigliaccio/U5W1L5/exceptions/BadRequestException.java
+++ src/main/java/giorgiomigliaccio/U5W1L5/exceptions/BadRequestException.java
@@ -1,13 +1,20 @@
 package giorgiomigliaccio.U5W1L5.exceptions;
 
-import java.time.LocalDateTime;
+import lombok.Getter;
+import org.springframework.validation.ObjectError;
 
+import java.util.List;
+
+@Getter
 public class BadRequestException extends RuntimeException {
+    private List<ObjectError> errorsList;
+
     public BadRequestException(String message) {
         super(message);
     }
 
-    public BadRequestException(String message, LocalDateTime now) {
+    public BadRequestException(List<ObjectError> errors) {
+        this.errorsList = errors;
     }
 }
 
